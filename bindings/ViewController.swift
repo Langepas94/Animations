2b39632b097468287738305de24f8bac8762f4e3
Artem <108129792+Langepas94@users.noreply.github.com>
2023-04-10 11:27:52 +0300

    Initial Commit

---
+++ bindings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bindings
+//
+//  Created by Artem on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
